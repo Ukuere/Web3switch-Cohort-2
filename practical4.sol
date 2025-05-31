f66ba1660910cca751ef3cc1826306f46c3573fb
Promise Ukuere <125465696+Ukuere@users.noreply.github.com>
2025-05-31 04:28:05 +0100

    Create practical4.sol
    Assignment

---
+++ practical4.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+// abstract contracts
+//  - not available on the contracts level
+//  - they cant be deployed on their own
+//  - they can have one functions, but will never be implemented
+//  - they serve as base contract for others to inherit from
+
+abstract contract Owner{
+…        return token;
+    }
+}
